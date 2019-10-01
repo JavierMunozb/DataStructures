18ea244ccc0e295b0f5024a4a70ad2360670c4d3
Javier Muñoz <javier.munozb@edu.uah.es>
2019-10-01 11:55:11 +0200

    Updated exercise 2

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+	int arrayOfInts[] = { 1, 2, 3, 5, 4, 6, 7, 8};
+	float arrayOfFloats[] = {12.4, 445.06, 32.4, 5555.55, 6666.66, 98.01};
+	char arrayOfChars[] = {'a','b','c','d','e','f'};
+	cout << arrayOfInts[4]; //<< ", " << arrayOfFloats[4] << ", " << arrayOfChars[3] << endl;
+	return 0;
 }
